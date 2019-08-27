7d9139a0283a4821b465fd5acc6f8995fca3ef43
Administrator <Administrator@192.168.12.57>
2019-08-27 17:54:58 +0200

    hola baby

--- WebContent/js/s47.js
+++ WebContent/js/s47.js
@@ -7,6 +7,6 @@ $(function() {
     console.log('Ready for JQuery');
     
     let score = 42;
-    $('textarea').append('Your score is ' + score + '\n');
+    $('target').append('Your score is ' + score + '\n');
 
 });
